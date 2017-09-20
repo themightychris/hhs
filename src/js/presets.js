2b904abe34bb186b045cbbadf12f89f83772f161
Ryon Coleman <ryon55@gmail.com>
2017-09-20 17:07:01 -0400

    Add clients and presets data

---
+++ src/js/presets.js
@@ -0,0 +1,68 @@
+var presets = {
+    "single": {
+        "user_type_jail": null,
+        "user_type_homeless": null,
+        "disabled": null,
+        "family_status": false,
+        "chronic": null,
+        "veteran": null,
+        "housing_status": null,
+        "release_date_start": null,
+        "release_date_end": null
+    },
+    "family": {
+        "user_type_jail": null,
+        "user_type_homeless": null,
+        "disabled": null,
+        "family_status": true,
+        "chronic": null,
+        "veteran": null,
+        "housing_status": null,
+        "release_date_start": null,
+        "release_date_end": null
+    },
+    "veteran": {
+        "user_type_jail": null,
+        "user_type_homeless": null,
+        "disabled": null,
+        "family_status": null,
+        "chronic": null,
+        "veteran": true,
+        "housing_status": null,
+        "release_date_start": null,
+        "release_date_end": null
+    },
+    "youth": {
+        "user_type_jail": null,
+        "user_type_homeless": null,
+        "disabled": null,
+        "family_status": null,
+        "chronic": null,
+        "veteran": null,
+        "housing_status": null,
+        "release_date_start": null,
+        "release_date_end": null
+    },
+    "chronic": {
+        "user_type_jail": null,
+        "user_type_homeless": null,
+        "disabled": null,
+        "family_status": null,
+        "chronic": true,
+        "veteran": null,
+        "housing_status": null,
+        "release_date_start": null,
+        "release_date_end": null
+    },
+    "reset": {
+        "user_type_jail": null,
+        "user_type_homeless": null,
+        "disabled": null,
+        "family_status": null,
+        "chronic": null,
+        "veteran": null,
+        "housing_status": null,
+        "release_date_start": null,
+        "release_date_end": null
+    }
+};
